f12242ec2c3efc3ba06ef446ab418edc7477c1d0
YAHYAOUI AYA <51344270+Akumaeye@users.noreply.github.com>
2023-02-02 10:49:38 +0100

    Update Banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,7 +1,7 @@
 import '../styles/Banner.css'
 function Banner(){
-    <div className='test-banner'>
+    return <div className='test-banner'>
     <h1>test-Banner</h1>
-</div>
+</div> 
 }
 export default Banner
